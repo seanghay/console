3c2130d621959b183baf21b28b985e1bda2d8c81
Yath Seanghay <seanghay.dev@gmail.com>
2020-01-03 11:57:01 +0700

    add spotless and bintray

--- spotless.license.kt
+++ spotless.license.kt
@@ -13,18 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.seanghay.console
-
-interface ConsoleLoggger {
-    fun log(vararg msg: Any)
-    fun info(vararg msg: Any)
-    fun warn(vararg msg: Any)
-    fun debug(vararg msg: Any)
-    fun error(vararg msg: Any)
-    fun time(tag: String)
-    fun timeEnd(tag: String)
-    fun count(tag: String)
-    fun countReset(tag: String)
-    fun tag(tag: String)
-    fun tagReset()
-}
